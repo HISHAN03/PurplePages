645080adf6357bb8b78e328453cf03d70094da3e
HISHAN03 <moidinhishan@gmail.com>
2024-01-27 10:47:35 +0530

    small-fixes-2

--- src/pages/api/dbconnect.ts
+++ src/pages/api/dbconnect.ts
@@ -2,8 +2,13 @@ import mongoose from 'mongoose'
 import { Processor } from 'postcss';
 const dbconnect=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL,{dbName:"perplepages"})
-        console.log("mongoose connected");
+        if(process.env.MONGO_URL)
+        {
+            await mongoose.connect(process.env.MONGO_URL,{dbName:"perplepages"})
+            console.log("mongoose connected");
+        }else{
+            console.log("error connecting to db")
+        }
     }catch(error)
     {
         console.error("error connecting to db ",error)
